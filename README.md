07a0cffcb41fd0bd2b9ea402785f3ba9cb249f46
2q45 <71389851+2q45@users.noreply.github.com>
2021-02-05 20:56:20 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Amoung-us-
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,11 @@
+var f;
+function setup() {
+  createCanvas(900,900);
+  f = new form();
+
+}
+
+function draw() {
+  background(255,255,255);  
+ f.display();
+}
